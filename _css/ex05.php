b2f76df0174fe49012ecb92b07c3b81fb95bee7d
AlmirNET <107294384+AlmirNet@users.noreply.github.com>
2022-12-23 21:44:48 -0300

    Operações aritméticas.
    Operações aritméticas.

---
+++ _css/ex05.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="_css/estilo.css">
+    <title></title>
+</head>
+<body>
+    <?php
+    $n1 = 3;
+    $n2 = 2;
+    $m = ($n1 + $n2) / 2;
+    echo "A soma vale ". ($n1+$n2);
+    echo "<br/>A subtracao vale " . ($n1-$n2);
+    echo "<br/>A multiplicacao vale " . ($n1*$n2);
+    echo "<br/>A divisao vale " . ($n1/$n2);
+    echo "<br/>O modulo vale " . ($n1%$n2);
+    echo "<br/>A média vale $m";   
+
+
+    ?>
+</body>
+</html>
---
+++ funçoesaritmeticas.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="_css/estilo.css">
+        <title>funcoes aritmeticas</title>
+        <style>
+            h2 {
+                font: ariel;
+                font-color
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+        <?php
+        $v1 = $_GET["x"];
+        $v2 = $_GET["y"];
+        echo "<h2>Valores Recebidos: $v1 e $v2<h2/>";
+        ?>
+    </div>    
+    </body>
+    
+</html>
